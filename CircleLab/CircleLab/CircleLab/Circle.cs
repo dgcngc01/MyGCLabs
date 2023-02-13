f7e2623dc2427fd82c4c838d4434679b68558477
Doug Coffey <dougcoffey@rocketmortgage.com>
2023-02-13 12:04:55 -0500

    circlelabtry1

---
+++ CircleLab/CircleLab/CircleLab/Circle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CircleLab;
+
+namespace CircleLab
+{
+    public class Circle
+    {
+        public static void GreetPerson()
+        {
+            Console.WriteLine("Hello, Welcome to Grand Circus' Circle Lab");
+            Console.WriteLine("Hope you are well.");
+        }
+        private float radius;
+        
+        public static double CalculateCircumference(double inputNumCirc)         // unformatted
+        { 
+        double circumference = 2 * Math.PI * inputNumCirc;
+        return circumference;
+        }
+
+        public static string CalculateFormattedCircumference(double inputNumCirc)
+        {
+            double circumference = 2 * Math.PI * inputNumCirc;
+            return circumference.ToString("n2");
+        }
+
+        public static double CalculateArea(double inputNumArea)         // unformatted
+        {
+            double calcArea = Math.PI * inputNumArea * inputNumArea;
+            return calcArea;
+        }
+
+        public static string CalculateFormattedArea(double inputNumArea)
+        {
+            double calcArea = Math.PI * inputNumArea * inputNumArea;
+            return calcArea.ToString("n2");
+        }
+
+        public static string FormattedNumber(double numberSent)
+        {
+            return numberSent.ToString("n2");
+        }
+
+
+        /*            
+                    public string CalculateFormattedCircumference()
+                { }
+                public double CalculateArea()
+                { }
+                public string CalculateFormattedArea()
+                { }
+                private string FormatNumber(double x)
+                { }
+                */
+
+    }
+}
---
+++ CircleLab/CircleLab/CircleLab/Program.cs
@@ -0,0 +1,56 @@
+﻿// Doug Coffey
+// Circle Lab
+
+using CircleLab;                             
+bool isDone = false;
+string userWantsToContinue = "";
+string userCategory = "";
+int radiusEntered = 0;
+bool isInt = false;
+int runCount = 1;
+
+Circle.GreetPerson();
+
+while (!isDone)
+{
+    Console.WriteLine("Enter radius: ");
+    string userRadius = Console.ReadLine();
+
+    isInt = int.TryParse(userRadius, out radiusEntered);
+
+    if (isInt)
+    {
+        double circUnformatted = Circle.CalculateCircumference(radiusEntered);
+        Console.WriteLine("\nCircumference is (unformatted): {0}", circUnformatted);
+        string circFormatted = Circle.CalculateFormattedCircumference(radiusEntered);
+        Console.WriteLine("Circumference is (unformatted): {0}", circFormatted);
+
+        double areaUnformatted = Circle.CalculateArea(radiusEntered);
+        Console.WriteLine("\nArea is (unformatted): {0}", areaUnformatted);
+        string areaFormatted = Circle.CalculateFormattedArea(radiusEntered);
+        Console.WriteLine("Area is (unformatted): {0}", areaFormatted);
+    }
+        
+    if (!isDone)
+    {
+        userWantsToContinue = "";
+        while (userWantsToContinue != "n" &&
+                userWantsToContinue != "y")
+        {
+            Console.WriteLine($"\nWould you like to continue? ('y'/'n')\n");
+            userWantsToContinue = Console.ReadLine().ToLower();
+
+            if (userWantsToContinue == "n")
+            {
+                isDone = true;
+                Console.WriteLine($"\nHave a great day. You created {runCount} Circle object(s).");
+            }
+            else if (userWantsToContinue == "y")
+            {
+                isDone = false;
+                isInt = false;
+                runCount++;
+            }
+        }
+    }
+}
