bff4ee750cb4091a995642b1a9943de168fd3927
Doug Coffey <dougcoffey@rocketmortgage.com>
2023-04-09 19:45:36 -0400

    CoffeeShopLab First Attempt

---
+++ CoffeeShopLab/CoffeeShopLab/Models/UserProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShopLab.Models
+{
+    public class UserProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
